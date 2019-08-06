ef36744eefbf746f03f8a3874dd58b6498ff1af2
Sam Yang <samuel.q.yang@gmail.com>
2019-08-06 15:46:53 -0700

    idek

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -15,7 +15,7 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      loggedIn: false,
+      loggedIn: true,
       name: "Tester"
     }
   }
--- front-end/src/components/user-app-components/AppAccount.js
+++ front-end/src/components/user-app-components/AppAccount.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import { Link } from "react-router-dom";
+import { TextInput, Button } from 'carbon-components-react'
 
 export default class AppAccount extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+
+        }
+    }
+
+    componentDidMount() {
+        //pull data from API
+        //alternatively user data can be sent in as props from UserApp
+    }
+    
     render() {
         return(
             <div id="app-account-div">
-               <h1>My Account</h1>
+               <h2>My Account</h2>
+               <div className="row">
+                <div className="col-md-6">
+                    <div id="account-profile" className="account-card">
+                        <h3>Profile</h3>
+                        <hr />
+                    </div>
+                </div>
+                <div className="col-md-6" id="account-info">
+                    <div id="account-payment" className="account-card">
+                        <h3>Payment Info</h3>
+                        <hr />
+                    </div>
+                    <div id="account-sensitive" className="account-card">
+                        <h3>Settings</h3>
+                        <hr />
+                    </div>
+                </div>
+               </div>
             </div>
         )
     }
